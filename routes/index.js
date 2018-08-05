0f6f86deb4190659a951426a7960093803ffc7af
sjlutterbie <simon@ltrbe.com>
2018-08-05 00:21:18 +0000

    EOD Commit: 2018-08-04

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'IO Demo' });
 });
 
+/* GET project page */
+router.get('/project', function(req,res,next) {
+   res.render('project', {title: 'Project title'}); 
+});
+
 module.exports = router;
